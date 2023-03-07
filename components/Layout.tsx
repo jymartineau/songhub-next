c227b89ac7de937c5ed0e3a71d2e5e964247bf6f
Kassim Machioudi <kassim@machioudi.com>
2023-03-07 15:36:09 +0100

    fix: Navbar removed SGH-17

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Navbar from './Navbar'
 import UserWatcher from './UserWatcher'
 
 function Layout({children}:{children?:React.ReactNode}) {
   return (
     <div>
       <UserWatcher />
-      <Navbar />
       {children}
     </div>
   )
